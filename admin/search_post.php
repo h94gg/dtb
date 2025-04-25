98577499b80be7dab4f71bfd5c8fce1d6ef318d6
hayder <118937855+h94gg@users.noreply.github.com>
2025-04-25 22:25:04 +0300

    Update search_post.php

--- admin/search_post.php
+++ admin/search_post.php
@@ -9,11 +9,11 @@
 session_start();
 
 // معلومات الاتصال بقاعدة البيانات
-$servername = "Driver={MySQL ODBC 8.0 UNICODE Driver};Server=MYSQL1002.site4now.net;Database=db_ab8100_dbeps;Uid=ab8100_dbeps;Password=BF2fHLwra@h4seY
-";
+$servername = "Server=MYSQL1002.site4now.net;Database=db_ab8100_hayder0;Uid=ab8100_hayder0;Pwd=BF2fHLwra@h4seY";
 $username = "root";
 $password = "19941994Ha";
 $dbname = "dbeps";
+
 
 // إنشاء الاتصال
 $conn = new mysqli($servername, $username, $password, $dbname);
